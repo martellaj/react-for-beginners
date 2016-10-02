598fa4563c642494707435f0853a1578e940132e
Joe Martella <martellaj@live.com>
2016-10-02 12:04:45 -0700

    Adds an event handler for StorePicker submit.
    - Creates a constructor for StorePicker component.
    - Grabs store name from StorePicker component.

--- catch-of-the-day/src/components/StorePicker/StorePicker.jsx
+++ catch-of-the-day/src/components/StorePicker/StorePicker.jsx
@@ -3,12 +3,29 @@ import { getFunName } from '../../helpers';
 import React from 'react';
 
 export default class StorePicker extends React.Component {
+    constructor() {
+        super();
+
+        // Bind component to custom component functions.
+        this.goToStore = this.goToStore.bind(this);
+    }
+
+    goToStore(event) {
+        event.preventDefault();
+
+        // Grab text from input box.
+        let storeName = this.storeInput.value;
+
+        // Transition to /store/:storeId.
+    }
+
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 { /* Weird JSX comment. */ }
                 <h2>Please enter a store:</h2>
-                <input type="text" required placeholder="Store name" defaultValue={getFunName()} />
+                <input type="text" required placeholder="Store name" defaultValue={getFunName()}
+                    ref={(input) => { this.storeInput = input; }} />
                 <button type="submit">Visit store</button>
             </form>
         );
